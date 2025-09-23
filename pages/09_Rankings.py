7017b8ef1ab14d3738dbc6f4c73c4d87d44ee057
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-23 16:48:33 -0400

    Update 09_Rankings.py

--- pages/09_Rankings.py
+++ pages/09_Rankings.py
@@ -263,7 +263,7 @@ if show_cur_ticker_hm:
           .mark_rect(stroke="#2b2f36", strokeWidth=0.6, strokeOpacity=0.95)
           .encode(
               x=alt.X("Timeframe:N", sort=["Current"],
-                      axis=alt.Axis(orient="top", title=None, labelAngle=0, labelPadding=8)),
+                      axis=alt.Axis(orient="top", title=None, labelAngle=0, labelFlush=False,labelPadding=8)),
               y=alt.Y("Ticker:N", sort=ticker_order,
                       axis=alt.Axis(title=None, labelLimit=160, labelOverlap=False)),
               color=alt.Color("score:Q",
