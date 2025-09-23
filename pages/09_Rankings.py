8b3d58a04226af345882272860959cbc2f044780
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-23 15:56:15 -0400

    Update 09_Rankings.py

--- pages/09_Rankings.py
+++ pages/09_Rankings.py
@@ -201,7 +201,7 @@ all_cats = sorted(list(set().union(
 )))
 ordered_cats = [c for c in preferred if c in all_cats] + [c for c in all_cats if c not in preferred]
 
-c1, c2, c3 = st.columns([0.45, 0.34, 0.25])
+c1, c2, c3 = st.columns([0.45, 0.50, 0.25])
 with c1:
     show_cur_ticker_hm = st.checkbox("Show per-ticker current heatmap (category)", value=False)
 with c2:
