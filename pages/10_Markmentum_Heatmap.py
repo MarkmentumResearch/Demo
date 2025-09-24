223738f010af4872656d04c6477bcf3abcbb89a1
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-24 10:10:05 -0400

    Update 10_Markmentum_Heatmap.py

--- pages/10_Markmentum_Heatmap.py
+++ pages/10_Markmentum_Heatmap.py
@@ -588,7 +588,7 @@ if show_ticker_hm:
                     title="Δ Score",
                     titleColor="#1a1a1a",
                     labelColor="#1a1a1a",
-                    gradientLength=180,
+                    gradientLength=275,
                     labelLimit=80,
                 ),
             ),
