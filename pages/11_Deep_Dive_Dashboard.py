83d9c4b886a51c6d15300fd581d60b0190fca224
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-24 14:36:15 -0400

    Update 11_Deep_Dive_Dashboard.py

--- pages/11_Deep_Dive_Dashboard.py
+++ pages/11_Deep_Dive_Dashboard.py
@@ -1479,7 +1479,7 @@ with col_left:
         if "active_ticker" not in st.session_state:
             st.session_state["active_ticker"] = DEFAULT_TICKER
 
-        SEARCH_BOX_WIDTH_PX = 475
+        SEARCH_BOX_WIDTH_PX = 450
         st.markdown(
             f"""
             <style>
