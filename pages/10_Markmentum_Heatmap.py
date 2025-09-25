505cb35dd72e438d52293e1682f6ab44b1d99b81
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-25 14:29:50 -0400

    Update 10_Markmentum_Heatmap.py

--- pages/10_Markmentum_Heatmap.py
+++ pages/10_Markmentum_Heatmap.py
@@ -598,7 +598,7 @@ if show_ticker_hm:
     )
 
     # --- Centered, snug card
-    pad_l, center_col, pad_r = st.columns([1, 3, 1])
+    pad_l, center_col, pad_r = st.columns([1.5, 3, 1])
     with center_col:
         with st.container(border=True):
             st.markdown(
