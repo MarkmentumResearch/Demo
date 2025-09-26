7d247163bffd602114cdeaa9153f301fedfb1eee
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-26 12:39:08 -0400

    Update 11_Deep_Dive_Dashboard.py

--- pages/11_Deep_Dive_Dashboard.py
+++ pages/11_Deep_Dive_Dashboard.py
@@ -1352,7 +1352,7 @@ def load_g24_ticker(path: Path, ticker: str,_mtime: float = last_modified) -> pd
 
 
 st.markdown('<div id="stat-center"></div>', unsafe_allow_html=True)
-_, mid_stat, _ = st.columns([2.2,3,.8])
+_, mid_stat, _ = st.columns([2.2,3,.6])
 
 with mid_stat:
 
