b7a1b3ff8d7c238ef387ce3910cd746782df042c
MarkmentumResearch <gary.markoski@gmail.com>
2025-09-26 11:28:21 -0400

    Update 11_Deep_Dive_Dashboard.py

--- pages/11_Deep_Dive_Dashboard.py
+++ pages/11_Deep_Dive_Dashboard.py
@@ -1649,7 +1649,7 @@ with mid_g1:
         rcParams["font.sans-serif"] = ["Segoe UI", "Arial", "Helvetica", "DejaVu Sans", "Liberation Sans", "sans-serif"]
 
         # Graph #1 – bigger and a bit taller
-        fig, ax = plt.subplots(figsize=(10.5, 6.2))
+        fig, ax = plt.subplots(figsize=(9, 5))
         fig.subplots_adjust(left=0.035, right=0.995, top=0.86, bottom=0.30)
         fig.set_facecolor("white")
         # Day PR (gray) lines
