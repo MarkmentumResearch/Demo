1c1d5ab3e02f93ae9457af375e9adbbd0516294b
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-10 16:45:20 -0400

    Update 07_Performance_Heatmap _v2.py

--- pages/07_Performance_Heatmap _v2.py
+++ pages/07_Performance_Heatmap _v2.py
@@ -263,7 +263,7 @@ heat = (
                  alt.Tooltip("n:Q", title="Count")]
     )
     .properties(width=chart_w, height=chart_h,
-                padding={"left": legend_w, "right": 0, "top": 6, "bottom": -40})
+                padding={"left": legend_w, "right": 0, "top": 6, "bottom": -30})
     .configure_view(strokeOpacity=0)
 )
 
