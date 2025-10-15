788dd159cf55075c90f2462f5ffc80feebd05ccf
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-15 18:14:09 -0400

    Update 07_Performance_Heatmap_v2.py

--- pages/07_Performance_Heatmap_v2.py
+++ pages/07_Performance_Heatmap_v2.py
@@ -344,7 +344,7 @@ with center_col:
 
 # ----- Left Card (static HTML like Compass) -----
 st.markdown('<div id="hm-center"></div>', unsafe_allow_html=True)
-col_card = st.columns(1)[0]
+left, col_card, right = st.columns([1, 2, 1])
 
 with col_card:
     tf_order = ["Daily", "WTD", "MTD", "QTD"]
