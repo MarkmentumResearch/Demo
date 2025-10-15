f33fd0b6d90b25528a3b2ad2a5df847ec94e38b3
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-15 17:05:00 -0400

    Update 07_Performance_Heatmap_v2.py

--- pages/07_Performance_Heatmap_v2.py
+++ pages/07_Performance_Heatmap_v2.py
@@ -106,7 +106,7 @@ st.markdown("""
 }
 
 /* % columns: make them compact and non-growing (about “00.00%” wide) */
-.tbl col.col-small { width:7.2ch; min-width:7.2ch; max-width:7.2ch; }
+.tbl col.col-small { width:15ch; min-width:15ch; max-width:15ch; }
 
             
 /* Hard fallback: fix widths by nth-child (2..5) in case a browser ignores colgroup */
@@ -114,7 +114,7 @@ st.markdown("""
 .tbl th:nth-child(3), .tbl td:nth-child(3),
 .tbl th:nth-child(4), .tbl td:nth-child(4),
 .tbl th:nth-child(5), .tbl td:nth-child(5) {
-  width:7.2ch; max-width:7.2ch;
+  width:15ch; max-width:15ch;
 }
 
 
