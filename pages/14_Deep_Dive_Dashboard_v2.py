58e5fbc43393a93d4870798fae6fb1b7a9038d9c
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-21 07:54:11 -0400

    Update 14_Deep_Dive_Dashboard_v2.py

--- pages/14_Deep_Dive_Dashboard_v2.py
+++ pages/14_Deep_Dive_Dashboard_v2.py
@@ -2051,7 +2051,7 @@ table.sp td:has(> .sp-badge.gray){{
   <table class="sp">
     <colgroup><col><col><col><col><col></colgroup>
     <thead>
-      <tr><th><th class="left">Signal</th></th><th>Daily Δ</th><th>WTD Δ</th><th>MTD Δ</th><th>QTD Δ</th></tr>
+      <tr><th></th><th>Daily Δ</th><th>WTD Δ</th><th>MTD Δ</th><th>QTD Δ</th></tr>
     </thead>
     <tbody>
       <tr>
@@ -2068,7 +2068,7 @@ table.sp td:has(> .sp-badge.gray){{
   <table class="sp sp-bias-lastcol">
     <colgroup><col><col><col><col><col><col><col></colgroup>
     <thead>
-      <tr><th><th class="left">Signal</th></th><th>ST</th><th>MT</th><th>LT</th><th>ST Δ</th><th>MT Δ</th><th>Tape Bias</th></tr>
+      <tr><th class="left">Signal</th><th>ST</th><th>MT</th><th>LT</th><th>ST Δ</th><th>MT Δ</th><th>Tape Bias</th></tr>
     </thead>
     <tbody>
       <tr>
