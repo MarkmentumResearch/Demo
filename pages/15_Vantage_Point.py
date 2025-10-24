91985334afcf1ca8aa9909c6316f7466f1e42f87
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-24 10:13:28 -0400

    Update 15_Vantage_Point.py

--- pages/15_Vantage_Point.py
+++ pages/15_Vantage_Point.py
@@ -344,11 +344,7 @@ _build_macro_card(sb)
 # =========================
 st.markdown("<br>", unsafe_allow_html=True)
 
-st.markdown("""
-<style>
-.tbl-cat th:nth-child(5), .tbl td:nth-child(5) { text-align:center; }
-</style>
-""", unsafe_allow_html=True)
+
 
 # Use the latest row per ticker (in case the CSV ever has multiple dates)
 sb["_dt"] = pd.to_datetime(sb["Date"], errors="coerce")
