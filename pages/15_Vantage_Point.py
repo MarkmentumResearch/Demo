a06fdf7493d99f00f949626aaa4d166df8541623
MarkmentumResearch <gary.markoski@gmail.com>
2025-10-24 10:10:31 -0400

    Update 15_Vantage_Point.py

--- pages/15_Vantage_Point.py
+++ pages/15_Vantage_Point.py
@@ -254,6 +254,7 @@ st.markdown("""
 .tbl-cat th:nth-child(4), .tbl-cat td:nth-child(4){
   border:none !important; background:transparent !important; padding:0 !important; /* spacer col */
 }
+.tbl-cat th:nth-child(5), .tbl td:nth-child(5) { text-align:center; }
 </style>
 """, unsafe_allow_html=True)
 
