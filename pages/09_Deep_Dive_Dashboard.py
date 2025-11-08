6b1871d82fef1c5c5f0df089390e763541294e2d
MarkmentumResearch <gary.markoski@gmail.com>
2025-11-07 19:04:49 -0500

    Update 09_Deep_Dive_Dashboard.py

--- pages/09_Deep_Dive_Dashboard.py
+++ pages/09_Deep_Dive_Dashboard.py
@@ -1715,7 +1715,7 @@ with mid_stat:
   </table>
 </div>
 """
-        st_html(html_doc, height=316, scrolling=False)
+        st_html(html_doc, height=319, scrolling=False)
 
     # ---------- type-ahead above the card ----------
     def render_ticker_typeahead_above(FILE_STATS: Path):
